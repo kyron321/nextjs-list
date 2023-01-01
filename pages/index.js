5cb31eb5cfbf8a6f18a88bdfeb35b4ee93475020
Kyron Smith <kds060902@outlook.com>
2023-01-01 21:03:28 +0000

    feat: added global styling and home page styling

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
-import Link from "next/link"
-import Footer from "../comps/footer"
-import Navbar from "../comps/Navbar"
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
-export default function Home(){
+export default function Home() {
   return (
     <div>
-
-      <h1>Home Page</h1>
-      <Link href = "/ninjas">See Ninja Listing</Link>
-
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi
+        impedit suscipit architecto, odio inventore nostrum non neque dicta.
+        Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem
+        nobis odit.
+      </p>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi
+        impedit suscipit architecto, odio inventore nostrum non neque dicta.
+        Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem
+        nobis odit.
+      </p>
+      <Link href="/ninjas/" className={styles.btn}>
+        See Ninja Listing
+      </Link>
     </div>
-  )
+  );
 }
